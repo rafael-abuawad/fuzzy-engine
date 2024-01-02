fc7ae542be96cfc7c4bdbd51d602fb5c93a85a09
Rafael Abuawad <rafael.abuawad@live.com>
2024-01-02 10:48:17 -0400

    Initial commit

---
+++ scripts/helper_config.py
@@ -0,0 +1,26 @@
+network_config = {
+    "ethereum": {
+        "local": {
+            "subscription_id": 0,
+            "key_hash": "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+            "update_interval": 60,
+            "fund_amount": 5_000000000000000000,
+        },
+        "mainnet-fork": {
+            "subscription_id": 0,
+            "key_hash": "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+            "update_interval": 60,
+            "fund_amount": 5_000000000000000000,
+        },
+        "sepolia": {
+            "subscription_id": 777,  # Add your own subscription Id here!
+            "key_hash": "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+            "update_interval": 60,
+            "fund_amount": 5_000000000000000000,
+        },
+    }
+}
+
+
+def main():
+    pass
